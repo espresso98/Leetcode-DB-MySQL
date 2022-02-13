1d103e49a66b022a66ef8ac8437222d50fa2eb92
Youra Cho <choyoura@gmail.com>
2022-02-13 00:07:31 -0800

    Add 1303. Find the Team Size.sql file

---
+++ 1303. Find the Team Size.sql
@@ -0,0 +1,30 @@
+-- Write an SQL query to find the team size of each of the employees.
+
+SELECT employee_id,
+   COUNT(employee_id) OVER (PARTITION BY team_id) AS team_size
+FROM Employee
+
+
+/* Input: 
+Employee Table:
++-------------+------------+
+| employee_id | team_id    |
++-------------+------------+
+|     1       |     8      |
+|     2       |     8      |
+|     3       |     8      |
+|     4       |     7      |
+|     5       |     9      |
+|     6       |     9      |
++-------------+------------+
+Output: 
++-------------+------------+
+| employee_id | team_size  |
++-------------+------------+
+|     1       |     3      |
+|     2       |     3      |
+|     3       |     3      |
+|     4       |     1      |
+|     5       |     2      |
+|     6       |     2      |
++-------------+------------+ */
