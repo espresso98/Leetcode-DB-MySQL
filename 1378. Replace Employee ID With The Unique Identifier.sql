83e5bc87552319275a00b5049db341ab191e46a6
Youra Cho <choyoura@gmail.com>
2022-02-19 22:30:58 -0800

    Add 1378. Replace Employee ID With The Unique Identifier.sql file

---
+++ 1378. Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,37 @@
+-- Write an SQL query to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+SELECT unique_id, name 
+FROM Employees  
+LEFT JOIN EmployeeUNI
+USING(id)
+
+
+/* Input: 
+Employees table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Alice    |
+| 7  | Bob      |
+| 11 | Meir     |
+| 90 | Winston  |
+| 3  | Jonathan |
++----+----------+
+EmployeeUNI table:
++----+-----------+
+| id | unique_id |
++----+-----------+
+| 3  | 1         |
+| 11 | 2         |
+| 90 | 3         |
++----+-----------+
+Output: 
++-----------+----------+
+| unique_id | name     |
++-----------+----------+
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
++-----------+----------+ */
