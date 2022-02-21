0e46d3815866260ed32f2c89eb5da6e1e3b3800d
Youra Cho <choyoura@gmail.com>
2022-02-20 22:21:25 -0800

    Add 1511. Customer Order Frequency.sql file

---
+++ 1511. Customer Order Frequency.sql
@@ -0,0 +1,50 @@
+-- Write an SQL query to report the customer_id and customer_name of customers who have spent at least $100 in each month of June and July 2020.
+
+SELECT customer_id, name
+FROM Customers
+JOIN Orders USING(customer_id)
+JOIN Product USING(product_id)
+WHERE YEAR(order_date) = 2020 AND MONTH(order_date) IN (6, 7)
+GROUP BY customer_id
+HAVING SUM(IF(MONTH(order_date)=6, price, 0) * quantity) >= 100
+    AND SUM(IF(MONTH(order_date)=7, price, 0) * quantity) >= 100
+
+
+
+/* Input: 
+Customers table:
++--------------+-----------+-------------+
+| customer_id  | name      | country     |
++--------------+-----------+-------------+
+| 1            | Winston   | USA         |
+| 2            | Jonathan  | Peru        |
+| 3            | Moustafa  | Egypt       |
++--------------+-----------+-------------+
+Product table:
++--------------+-------------+-------------+
+| product_id   | description | price       |
++--------------+-------------+-------------+
+| 10           | LC Phone    | 300         |
+| 20           | LC T-Shirt  | 10          |
+| 30           | LC Book     | 45          |
+| 40           | LC Keychain | 2           |
++--------------+-------------+-------------+
+Orders table:
++--------------+-------------+-------------+-------------+-----------+
+| order_id     | customer_id | product_id  | order_date  | quantity  |
++--------------+-------------+-------------+-------------+-----------+
+| 1            | 1           | 10          | 2020-06-10  | 1         |
+| 2            | 1           | 20          | 2020-07-01  | 1         |
+| 3            | 1           | 30          | 2020-07-08  | 2         |
+| 4            | 2           | 10          | 2020-06-15  | 2         |
+| 5            | 2           | 40          | 2020-07-01  | 10        |
+| 6            | 3           | 20          | 2020-06-24  | 2         |
+| 7            | 3           | 30          | 2020-06-25  | 2         |
+| 9            | 3           | 30          | 2020-05-08  | 3         |
++--------------+-------------+-------------+-------------+-----------+
+Output: 
++--------------+------------+
+| customer_id  | name       |  
++--------------+------------+
+| 1            | Winston    |
++--------------+------------+ */ 
