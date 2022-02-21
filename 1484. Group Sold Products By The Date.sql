b5bdef282c5dc6961f7c276c43b5d9a4eab08087
Youra Cho <choyoura@gmail.com>
2022-02-20 22:20:24 -0800

    Add 1484. Group Sold Products By The Date.sql file

---
+++ 1484. Group Sold Products By The Date.sql
@@ -0,0 +1,32 @@
+-- Write an SQL query to find for each date the number of different products sold and their names.
+-- The sold products names for each date should be sorted lexicographically.Return the result table ordered by sell_date
+
+SELECT sell_date, 
+       COUNT(DISTINCT product) AS num_sold,
+       GROUP_CONCAT(DISTINCT product ORDER BY product) AS products 
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
+
+
+/* Input: 
+Activities table:
++------------+------------+
+| sell_date  | product     |
++------------+------------+
+| 2020-05-30 | Headphone  |
+| 2020-06-01 | Pencil     |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | Basketball |
+| 2020-06-01 | Bible      |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | T-Shirt    |
++------------+------------+
+Output: 
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+  */ 
