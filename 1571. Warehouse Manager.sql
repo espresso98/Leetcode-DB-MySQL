1af54cbdeeb288b18dcfe71758d9698af2255a72
Youra Cho <choyoura@gmail.com>
2022-02-25 17:00:24 -0800

    Add 1571. Warehouse Manager.sql file

---
+++ 1571. Warehouse Manager.sql
@@ -0,0 +1,37 @@
+-- Write an SQL query to report the number of cubic feet of volume the inventory occupies in each warehouse.
+
+SELECT name AS warehouse_name, SUM((width*length*height)*units) AS volume
+FROM Warehouse 
+JOIN Products USING(product_id)
+GROUP BY name
+
+
+/* Input: 
+Warehouse table:
++------------+--------------+-------------+
+| name       | product_id   | units       |
++------------+--------------+-------------+
+| LCHouse1   | 1            | 1           |
+| LCHouse1   | 2            | 10          |
+| LCHouse1   | 3            | 5           |
+| LCHouse2   | 1            | 2           |
+| LCHouse2   | 2            | 2           |
+| LCHouse3   | 4            | 1           |
++------------+--------------+-------------+
+Products table:
++------------+--------------+------------+----------+-----------+
+| product_id | product_name | Width      | Length   | Height    |
++------------+--------------+------------+----------+-----------+
+| 1          | LC-TV        | 5          | 50       | 40        |
+| 2          | LC-KeyChain  | 5          | 5        | 5         |
+| 3          | LC-Phone     | 2          | 10       | 10        |
+| 4          | LC-T-Shirt   | 4          | 10       | 20        |
++------------+--------------+------------+----------+-----------+
+Output: 
++----------------+------------+
+| warehouse_name | volume     | 
++----------------+------------+
+| LCHouse1       | 12250      | 
+| LCHouse2       | 20250      |
+| LCHouse3       | 800        |
++----------------+------------+  */ 
