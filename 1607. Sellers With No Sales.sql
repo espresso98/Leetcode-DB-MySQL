bf1c4cdf2bb91d90b34b6e9f2fe02857679dee16
Youra Cho <choyoura@gmail.com>
2022-02-25 17:07:19 -0800

    Add 1607. Sellers With No Sales.sql file

---
+++ 1607. Sellers With No Sales.sql
@@ -0,0 +1,45 @@
+-- Write an SQL query to report the names of all sellers who did not make any sales in 2020.
+-- Return the result table ordered by seller_name in ascending order.
+ 
+
+SELECT seller_name
+FROM Seller
+WHERE seller_id NOT IN (
+    SELECT seller_id FROM Orders
+    WHERE YEAR(sale_date) = 2020)
+ORDER BY seller_name
+
+
+/* Input: 
+Customer table:
++--------------+---------------+
+| customer_id  | customer_name |
++--------------+---------------+
+| 101          | Alice         |
+| 102          | Bob           |
+| 103          | Charlie       |
++--------------+---------------+
+Orders table:
++-------------+------------+--------------+-------------+-------------+
+| order_id    | sale_date  | order_cost   | customer_id | seller_id   |
++-------------+------------+--------------+-------------+-------------+
+| 1           | 2020-03-01 | 1500         | 101         | 1           |
+| 2           | 2020-05-25 | 2400         | 102         | 2           |
+| 3           | 2019-05-25 | 800          | 101         | 3           |
+| 4           | 2020-09-13 | 1000         | 103         | 2           |
+| 5           | 2019-02-11 | 700          | 101         | 2           |
++-------------+------------+--------------+-------------+-------------+
+Seller table:
++-------------+-------------+
+| seller_id   | seller_name |
++-------------+-------------+
+| 1           | Daniel      |
+| 2           | Elizabeth   |
+| 3           | Frank       |
++-------------+-------------+
+Output: 
++-------------+
+| seller_name |
++-------------+
+| Frank       |
++-------------+ */ 
