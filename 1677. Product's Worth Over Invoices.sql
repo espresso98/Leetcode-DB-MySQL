74ee2387257c8291a2245557045a41fc9ac7c9dc
Youra Cho <choyoura@gmail.com>
2022-02-27 23:09:53 -0800

    Add 1677. Product's Worth Over Invoices.sql file

---
+++ 1677. Product's Worth Over Invoices.sql
@@ -0,0 +1,36 @@
+-- Write an SQL query that will, for all products, return each product name with the total amount due, paid, canceled, and refunded across all invoices.
+-- Return the result table ordered by product_name.
+
+SELECT name, IFNULL(SUM(rest), 0) rest, IFNULL(SUM(paid), 0) paid, IFNULL(SUM(canceled), 0) canceled, IFNULL(SUM(refunded), 0) refunded
+FROM Product 
+LEFT JOIN Invoice USING(product_id)
+GROUP BY 1
+ORDER BY 1
+
+
+/* Input: 
+Product table:
++------------+-------+
+| product_id | name  |
++------------+-------+
+| 0          | ham   |
+| 1          | bacon |
++------------+-------+
+Invoice table:
++------------+------------+------+------+----------+----------+
+| invoice_id | product_id | rest | paid | canceled | refunded |
++------------+------------+------+------+----------+----------+
+| 23         | 0          | 2    | 0    | 5        | 0        |
+| 12         | 0          | 0    | 4    | 0        | 3        |
+| 1          | 1          | 1    | 1    | 0        | 1        |
+| 2          | 1          | 1    | 0    | 1        | 1        |
+| 3          | 1          | 0    | 1    | 1        | 1        |
+| 4          | 1          | 1    | 1    | 1        | 0        |
++------------+------------+------+------+----------+----------+
+Output: 
++-------+------+------+----------+----------+
+| name  | rest | paid | canceled | refunded |
++-------+------+------+----------+----------+
+| bacon | 3    | 3    | 3        | 3        |
+| ham   | 2    | 4    | 5        | 3        |
++-------+------+------+----------+----------+ */
