152ac5a79c59c19ebb7dd047c8bc3e43a7905d4e
Youra Cho <choyoura@gmail.com>
2022-02-27 22:57:10 -0800

    Add 1633. Percentage of Users Attended a Contest.sql file

---
+++ 1633. Percentage of Users Attended a Contest.sql
@@ -0,0 +1,47 @@
+-- Write an SQL query to find the percentage of the users registered in each contest rounded to two decimals.
+-- Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+
+SELECT contest_id, ROUND(COUNT(user_id) * 100 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register 
+GROUP BY 1
+ORDER BY 2 DESC, 1 ASC
+
+
+
+/* Input: 
+Users table:
++---------+-----------+
+| user_id | user_name |
++---------+-----------+
+| 6       | Alice     |
+| 2       | Bob       |
+| 7       | Alex      |
++---------+-----------+
+Register table:
++------------+---------+
+| contest_id | user_id |
++------------+---------+
+| 215        | 6       |
+| 209        | 2       |
+| 208        | 2       |
+| 210        | 6       |
+| 208        | 6       |
+| 209        | 7       |
+| 209        | 6       |
+| 215        | 7       |
+| 208        | 7       |
+| 210        | 2       |
+| 207        | 2       |
+| 210        | 7       |
++------------+---------+
+Output: 
++------------+------------+
+| contest_id | percentage |
++------------+------------+
+| 208        | 100.0      |
+| 209        | 100.0      |
+| 210        | 100.0      |
+| 215        | 66.67      |
+| 207        | 33.33      |
++------------+------------+ */ 
