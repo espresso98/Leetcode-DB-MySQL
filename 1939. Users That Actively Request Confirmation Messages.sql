abee4ca46344c1a7277aec6aa864d2eb5300aae2
Youra Cho <choyoura@gmail.com>
2022-03-07 22:35:39 -0800

    Add 1939. Users That Actively Request Confirmation Messages.sql file

---
+++ 1939. Users That Actively Request Confirmation Messages.sql
@@ -0,0 +1,42 @@
+-- Write an SQL query to find the IDs of the users that requested a confirmation message twice within a 24-hour window. Two messages exactly 24 hours apart are considered to be within the window. The action does not affect the answer, only the request time.
+
+
+SELECT DISTINCT c1.user_id
+FROM Confirmations c1
+JOIN Confirmations c2
+ON c1.user_id = c2.user_id AND c1.time_stamp < c2.time_stamp
+WHERE c2.time_stamp between c1.time_stamp AND c1.time_stamp + INTERVAL 24 HOUR
+ORDER BY 1
+
+
+/* Input: 
+Signups table:
++---------+---------------------+
+| user_id | time_stamp          |
++---------+---------------------+
+| 3       | 2020-03-21 10:16:13 |
+| 7       | 2020-01-04 13:57:59 |
+| 2       | 2020-07-29 23:09:44 |
+| 6       | 2020-12-09 10:39:37 |
++---------+---------------------+
+Confirmations table:
++---------+---------------------+-----------+
+| user_id | time_stamp          | action    |
++---------+---------------------+-----------+
+| 3       | 2021-01-06 03:30:46 | timeout   |
+| 3       | 2021-01-06 03:37:45 | timeout   |
+| 7       | 2021-06-12 11:57:29 | confirmed |
+| 7       | 2021-06-13 11:57:30 | confirmed |
+| 2       | 2021-01-22 00:00:00 | confirmed |
+| 2       | 2021-01-23 00:00:00 | timeout   |
+| 6       | 2021-10-23 14:14:14 | confirmed |
+| 6       | 2021-10-24 14:14:13 | timeout   |
++---------+---------------------+-----------+
+Output: 
++---------+
+| user_id |
++---------+
+| 2       |
+| 3       |
+| 6       |
++---------+ */ 
