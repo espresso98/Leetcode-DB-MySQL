d7f1b44e8d69cd85eeef59f5afb9ad05abe93fd0
Youra Cho <choyoura@gmail.com>
2022-03-13 20:53:03 -0700

    Add 184. Department Highest Salary.sql file

---
+++ 184. Department Highest Salary.sql
@@ -0,0 +1,37 @@
+-- Write an SQL query to find employees who have the highest salary in each of the departments.
+
+SELECT d.name AS 'Department', e.name AS 'Employee', salary
+FROM Employee e
+JOIN Department d ON e.DepartmentId=d.Id 
+WHERE (departmentId, salary) IN 
+        (SELECT departmentId, MAX(salary) 
+         FROM Employee 
+         GROUP BY departmentId) 
+
+
+/* Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+ */ 
