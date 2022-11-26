b44572b742a1a76bb2a8d8beddbbf3e03c9acf96
Youra Cho <choyoura@gmail.com>
2022-11-26 15:04:33 -0800

    Update 183. Customers Who Never Order.sql

--- 183. Customers Who Never Order.sql
+++ 183. Customers Who Never Order.sql
@@ -1,6 +1,12 @@
+-- Solution 1
 SELECT name Customers
 FROM Customers
 WHERE id NOT IN (
     SELECT CustomerId
     FROM Orders)
     
+-- Solution 2
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o ON c.id = o.customerId
+WHERE o.customerId IS NULL
