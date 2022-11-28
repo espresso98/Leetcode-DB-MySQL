a4d10fb439a158524ac171df8908c7464dba5e24
Youra Cho <choyoura@gmail.com>
2022-11-28 09:50:48 -0800

    Add 1158. Market Analysis I.sql

---
+++ 1158. Market Analysis I.sql
@@ -0,0 +1,52 @@
+-- Write an SQL query to find for each user, the join date and the number of orders they made as a buyer in 2019.
+
+SELECT u.user_id AS buyer_id, join_date, COUNT(order_date) AS orders_in_2019
+FROM  Users u 
+LEFT JOIN Orders o ON u.user_id = o.buyer_id AND LEFT(order_date, 4) = '2019'
+GROUP BY 1
+ORDER BY 1
+
+
+```sql
+Example 1:
+
+Input: 
+Users table:
++---------+------------+----------------+
+| user_id | join_date  | favorite_brand |
++---------+------------+----------------+
+| 1       | 2018-01-01 | Lenovo         |
+| 2       | 2018-02-09 | Samsung        |
+| 3       | 2018-01-19 | LG             |
+| 4       | 2018-05-21 | HP             |
++---------+------------+----------------+
+Orders table:
++----------+------------+---------+----------+-----------+
+| order_id | order_date | item_id | buyer_id | seller_id |
++----------+------------+---------+----------+-----------+
+| 1        | 2019-08-01 | 4       | 1        | 2         |
+| 2        | 2018-08-02 | 2       | 1        | 3         |
+| 3        | 2019-08-03 | 3       | 2        | 3         |
+| 4        | 2018-08-04 | 1       | 4        | 2         |
+| 5        | 2018-08-04 | 1       | 3        | 4         |
+| 6        | 2019-08-05 | 2       | 2        | 4         |
++----------+------------+---------+----------+-----------+
+Items table:
++---------+------------+
+| item_id | item_brand |
++---------+------------+
+| 1       | Samsung    |
+| 2       | Lenovo     |
+| 3       | LG         |
+| 4       | HP         |
++---------+------------+
+Output: 
++-----------+------------+----------------+
+| buyer_id  | join_date  | orders_in_2019 |
++-----------+------------+----------------+
+| 1         | 2018-01-01 | 1              |
+| 2         | 2018-02-09 | 2              |
+| 3         | 2018-01-19 | 0              |
+| 4         | 2018-05-21 | 0              |
++-----------+------------+----------------+
+```
