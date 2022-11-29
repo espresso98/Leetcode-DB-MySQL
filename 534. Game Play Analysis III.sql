066a3f26b2f7a9d62288aad1a2644ceef6195d58
Youra Cho <choyoura@gmail.com>
2022-11-29 09:46:17 -0800

    Add 534. Game Play Analysis III.sql

---
+++ 534. Game Play Analysis III.sql
@@ -0,0 +1,31 @@
+-- Write an SQL query to report for each player and date, how many games played so far by the player. 
+-- That is, the total number of games played by the player until that date.
+
+SELECT player_id, event_date,
+    SUM(games_played) OVER(PARTITION BY player_id ORDER BY event_date) AS games_played_so_far
+FROM Activity
+
+
+/* 
+Input: 
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-05-02 | 6            |
+| 1         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output: 
++-----------+------------+---------------------+
+| player_id | event_date | games_played_so_far |
++-----------+------------+---------------------+
+| 1         | 2016-03-01 | 5                   |
+| 1         | 2016-05-02 | 11                  |
+| 1         | 2017-06-25 | 12                  |
+| 3         | 2016-03-02 | 0                   |
+| 3         | 2018-07-03 | 5                   |
++-----------+------------+---------------------+
+*/
