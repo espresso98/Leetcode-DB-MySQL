3372b63b50e96fddf4b84b8c5c440a17d26f2f49
Youra Cho <choyoura@gmail.com>
2022-12-02 09:31:10 -0800

    Add 1141. User Activity for the Past 30 Days I.sql

--- 1141. User Activity for the Past 30 Days I.sql
+++ 1141. User Activity for the Past 30 Days I.sql
@@ -9,6 +9,6 @@ GROUP BY activity_date
 --Solution2
 SELECT activity_date as day, COUNT(DISTINCT user_id) as active_users
 FROM Activity
-WHERE DATEDIFF('2019-07-27', activity_date) < 30
+WHERE activity_date <='2019-07-27' AND DATEDIFF('2019-07-27', activity_date) < 30
 GROUP BY activity_date
 HAVING COUNT(activity_type) >= 1
