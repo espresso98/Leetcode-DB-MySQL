4f43a6971d55387938b377ac8f26dfbff479afca
Youra Cho <choyoura@gmail.com>
2022-12-23 22:40:56 -0800

    Add 1596. The Most Frequently Ordered Products for Each Customer.sql

---
+++ 1596. The Most Frequently Ordered Products for Each Customer.sql
@@ -0,0 +1,63 @@
+WITH freq_rank AS (
+    SELECT o.customer_id, o.product_id, p.product_name, 
+    RANK() OVER (PARTITION BY customer_id ORDER BY COUNT(o.product_id) DESC) AS rnk
+    FROM Orders o
+    JOIN Products p
+    ON O.product_id = P.product_id  
+    GROUP BY customer_id, product_id
+)
+SELECT customer_id, product_id, product_name
+FROM freq_rank
+WHERE rnk = 1 
+ORDER BY customer_id, product_id
+
+/* Input: 
+Customers table:
++-------------+-------+
+| customer_id | name  |
++-------------+-------+
+| 1           | Alice |
+| 2           | Bob   |
+| 3           | Tom   |
+| 4           | Jerry |
+| 5           | John  |
++-------------+-------+
+
+Orders table:
++----------+------------+-------------+------------+
+| order_id | order_date | customer_id | product_id |
++----------+------------+-------------+------------+
+| 1        | 2020-07-31 | 1           | 1          |
+| 2        | 2020-07-30 | 2           | 2          |
+| 3        | 2020-08-29 | 3           | 3          |
+| 4        | 2020-07-29 | 4           | 1          |
+| 5        | 2020-06-10 | 1           | 2          |
+| 6        | 2020-08-01 | 2           | 1          |
+| 7        | 2020-08-01 | 3           | 3          |
+| 8        | 2020-08-03 | 1           | 2          |
+| 9        | 2020-08-07 | 2           | 3          |
+| 10       | 2020-07-15 | 1           | 2          |
++----------+------------+-------------+------------+
+
+Products table:
++------------+--------------+-------+
+| product_id | product_name | price |
++------------+--------------+-------+
+| 1          | keyboard     | 120   |
+| 2          | mouse        | 80    |
+| 3          | screen       | 600   |
+| 4          | hard disk    | 450   |
++------------+--------------+-------+
+
+Output: 
++-------------+------------+--------------+
+| customer_id | product_id | product_name |
++-------------+------------+--------------+
+| 1           | 2          | mouse        |
+| 2           | 1          | keyboard     |
+| 2           | 2          | mouse        |
+| 2           | 3          | screen       |
+| 3           | 3          | screen       |
+| 4           | 1          | keyboard     |
++-------------+------------+--------------+
+*/
