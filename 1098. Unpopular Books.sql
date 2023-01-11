46963c441034b743957601f7fc330963b8c404fd
Youra Cho <choyoura@gmail.com>
2023-01-10 23:49:05 -0800

    Add 1098. Unpopular Books.sql

---
+++ 1098. Unpopular Books.sql
@@ -0,0 +1,9 @@
+-- Write an SQL query that reports the books that have sold less than 10 copies in the last year, excluding books that have been available for less than one month from today. Assume today is 2019-06-23
+SELECT book_id, name
+FROM Books
+WHERE available_from < DATE_SUB('2019-06-23', INTERVAL 1 MONTH)
+AND book_id NOT IN (
+    SELECT book_id FROM Orders 
+    WHERE dispatch_date BETWEEN '2018-06-23' AND '2019-06-23'
+    GROUP BY book_id
+    HAVING SUM(quantity) >= 10)
